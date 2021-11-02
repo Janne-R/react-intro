a678a80a9dccef3a366e5196cea084d18f4f052c
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-11-02 11:06:20 +0100

    sticky footer

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import FirstParagraph from './components/FirstParagraph';
 import SubHeading from './components/SubHeading';
 import Title from './components/Title';
 import SecondParagraph from './components/SecondParagraph';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="container">
-      <div>
-        <Heading />
-        <SubHeading />
-        <FirstParagraph />
+    <>
+      <div className="wrapper">
+        <div className="container">
+          <div>
+            <Heading />
+            <SubHeading />
+            <FirstParagraph />
+          </div>
+          <div className="card">
+            <Title />
+            <SecondParagraph />
+          </div>
+        </div>
       </div>
-      <div className="card">
-
-        <Title />
-
-        <SecondParagraph />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+  return <footer>
+    <p>Left footer</p>
+    <p>Right footer</p>
+  </footer>
+}
+
+export default Footer;
